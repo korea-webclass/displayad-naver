5eea702a3169b315cb7d8bb0414e68fcafb6a208
webclass <korea.webclass@gmail.com>
2021-02-16 21:13:06 +0900

    쉘 스크립트 삭제

--- change_author.sh
+++ change_author.sh
@@ -7,4 +7,4 @@ export GIT_COMMITTER_NAME="$NEW_NAME"
 export GIT_COMMITTER_EMAIL="$NEW_EMAIL" 
 export GIT_AUTHOR_NAME="$NEW_NAME" 
 export GIT_AUTHOR_EMAIL="$NEW_EMAIL" 
-'--tag-name-filter cat -- --branches --tags
+'--tag-name-filter cat -- --branches --tags -f
