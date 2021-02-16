42f15b314303c51f869a1ccae4d919c6a3ac34dc
webclass <korea.webclass@gmail.com>
2021-02-16 18:43:57 +0900

    테스트

--- change_author.sh
+++ change_author.sh
@@ -1,3 +1,1 @@
 git filter-branch --env-filter ' WRONG_EMAIL="whwns940@gmail.com" NEW_NAME="webclass" NEW_EMAIL="korea.webclass@gmail.com" if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ] then export GIT_COMMITTER_NAME="$NEW_NAME" export GIT_COMMITTER_EMAIL="$NEW_EMAIL" fi if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ] then export GIT_AUTHOR_NAME="$NEW_NAME" export GIT_AUTHOR_EMAIL="$NEW_EMAIL" fi ' --tag-name-filter cat -- --branches --tags
-
-출처: https://jhrun.tistory.com/276 [JHRunning]
