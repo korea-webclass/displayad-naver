b958fe386fc366b879e77fa8435f3cfc0e020d8d
webclass <korea.webclass@gmail.com>
2021-03-02 16:58:58 +0900

    헤더 영역 JS 구현 mouseenter, mouseleave 사용 / html 태그 수정

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 window.onload = function () {
     const htmlElem = document.querySelector('html');
     const header = htmlElem.querySelector('.header');
-    const gnb = htmlElem.querySelector('.gnb');
-    const gnbSub = htmlElem.querySelector('.sub');
-    let on;
-
+    // const gnb = htmlElem.querySelector('.gnb');
+    const gnbLi = htmlElem.querySelectorAll('.gnb .item');
+    // const gnbSub = htmlElem.querySelector('.sub');
     // 헤더 스크롤
     window.addEventListener('scroll', scroll);
     function scroll() {
@@ -16,53 +15,29 @@ window.onload = function () {
     };
 
     // 헤더 서브메뉴
-    gnb.addEventListener('mouseover', over);
-    gnb.addEventListener('mouseout', out);
-    gnbSub.addEventListener('mouseout', gnbSubOut);
+    for (let i = 0; i < gnbLi.length; i++) {
+        gnbLi[i].addEventListener('mouseenter', enter);
+        gnbLi[i].addEventListener('mouseleave', leave);
+    }
 
-    function over(e) {
-        let a = e.target;
-        let li = a.parentNode;
-        let sub = li.querySelector('.sub')
-        if (a.tagName !== 'A') return;
-        if (sub) {
-            sub.classList.add('on');
+    function enter(e) {
+        let li = e.target;
+        let gnbSub = li.querySelector('.sub');
+        if (gnbSub) {
+            gnbSub.classList.add('on');
             header.classList.add('sub');
-            on = sub;
         }
     }
 
-    function out(e) {
-        let a = e.target;
-        let li = a.parentNode;
-        let sub = li.querySelector('.sub')
-        if (a.tagName !== 'A') return;
-        if (onOff(e.relatedTarget)) {
-            sub.classList.remove('on');
-            // header.classList.remove('sub');
+    function leave(e) {
+        let li = e.target;
+        let gnbSub = li.querySelector('.sub');
+        if (gnbSub) {
+            gnbSub.classList.remove('on');
+            header.classList.remove('sub');
         }
     }
 
-    function gnbSubOut(e) {
-        let a = e.target;
-        if (a.tagName !== 'A') return;
-        console.log(a)
-    }
-
-    function onOff(relatedTarget) {
-        console.log(relatedTarget);
-        while (relatedTarget.tagName !== 'HTML') {
-            console.log(relatedTarget);
-            if (relatedTarget === on.parentNode) {
-                return false;
-            }
-            // console.log(relatedTarget);
-            relatedTarget = relatedTarget.parentNode;
-            // console.log(relatedTarget);
-        }
-        return true
-    }
-    
 
 
 
@@ -74,19 +49,8 @@ window.onload = function () {
 
 
 
-    // relatedTarget는 마우스가 나간 위치의 객체를 줌
-    // 너가 도착한 곳 태그명이 HTML이 아니잖어? 응! relatedTarget parentNode
-    
 
-
-
-
-
-
-
-
-
-
+    // 헤더 - 서브메뉴 다 나오는 코드
     // for (let i = 0; i < mainMenuList.length; i++) {
     //     mainMenuList[i].addEventListener('mouseover', function () {
     //         subMenuHeight = this.querySelector('ul').offsetHeight;
