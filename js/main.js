9db8e62c28d6bd4098c181dd96591d16bd0afe40
webclass <korea.webclass@gmail.com>
2021-03-04 22:52:06 +0900

    swiper.js 슬라이드 영역 이동, 카운트 다운 애니메이션 작업 및 기타

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ window.onload = function () {
     // const gnb = htmlElem.querySelector('.gnb');
     const gnbLi = htmlElem.querySelectorAll('.gnb .item');
     // const gnbSub = htmlElem.querySelector('.sub');
+    const count = document.querySelector('.main .ad-data-content .count');
+
+
+
+
+
     // 헤더 스크롤
     window.addEventListener('scroll', scroll);
     function scroll() {
@@ -38,7 +44,42 @@ window.onload = function () {
         }
     }
 
+    // swiper.js 슬라이드 영역
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
+    // 카운트 다운 애니메이션
+    var memberCountConTxt= 30000000;
+
+    $({val: 0}).animate({val: memberCountConTxt}, {
+        duration: 2000,
+        step: function() {
+            var num = numberWithCommas(Math.floor(this.val));
+            $(".memberCountCon").text(num);
+        },
+        complete: function() {
+            var num = numberWithCommas(Math.floor(this.val));
+            $(".memberCountCon").text(num);
+        }
+    });
+  
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
 
 
