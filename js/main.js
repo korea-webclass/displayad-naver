e823bd415a7a8e9cfff0eeb434d67893c9f10851
webclass <korea.webclass@gmail.com>
2021-03-17 19:22:22 +0900

    fix : 공통 헤더 서브메뉴 및 비주얼 영역 타이틀

--- js/main.js
+++ js/main.js
@@ -17,28 +17,38 @@ window.onload = function () {
     };
 
     // 헤더 서브메뉴
-    for (let i = 0; i < gnbLi.length; i++) {
-        gnbLi[i].addEventListener('mouseenter', enter);
-        gnbLi[i].addEventListener('mouseleave', leave);
-    }
+    $(function(){
+        $("ul.sub").hide();
+        $(".gnb > .item").hover(function () {
+            $("ul:not(:animated)",this).slideDown("fast");
+        },
+        function () {
+            $("ul",this).slideUp("fast");
+        });
+    });
+    
+    // for (let i = 0; i < gnbLi.length; i++) {
+    //     gnbLi[i].addEventListener('mouseenter', enter);
+    //     gnbLi[i].addEventListener('mouseleave', leave);
+    // }
 
-    function enter(e) {
-        let li = e.target;
-        let gnbSub = li.querySelector('.sub');
-        if (gnbSub) {
-            gnbSub.classList.add('on');
-            header.classList.add('sub');
-        }
-    }
+    // function enter(e) {
+    //     let li = e.target;
+    //     let gnbSub = li.querySelector('.sub');
+    //     if (gnbSub) {
+    //         gnbSub.classList.add('on');
+    //         header.classList.add('sub');
+    //     }
+    // }
 
-    function leave(e) {
-        let li = e.target;
-        let gnbSub = li.querySelector('.sub');
-        if (gnbSub) {
-            gnbSub.classList.remove('on');
-            header.classList.remove('sub');
-        }
-    }
+    // function leave(e) {
+    //     let li = e.target;
+    //     let gnbSub = li.querySelector('.sub');
+    //     if (gnbSub) {
+    //         gnbSub.classList.remove('on');
+    //         header.classList.remove('sub');
+    //     }
+    // }
 
     // 헤더 우측 메뉴
     detailUtil.addEventListener('mouseenter', function () {
