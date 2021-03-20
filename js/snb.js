ab8697f0e0e7e37a671dc0424628e701c71cf705
webclass <korea.webclass@gmail.com>
2021-03-20 22:01:16 +0900

    fix : 헤더 서브메뉴, 경로, HTML 태그

--- js/snb.js
+++ js/snb.js
@@ -1,43 +1,17 @@
 window.onload = function () {
-    const htmlElem = document.querySelector('html');
-    const header = document.querySelector('.header');
-    const gnbLi = document.querySelectorAll('.gnb .item');
     const detailUtil = document.querySelector('.detail-util');
     const utilMenu = document.querySelector('.detail-util .menu');
     
-    // 헤더 스크롤 고정
-    window.addEventListener('scroll', scroll);
-    function scroll() {
-        if (htmlElem.scrollTop > 30) {
-            header.classList.remove('on');
-        } else {
-            header.classList.add('on');
-        }
-    };
-
     // 헤더 서브메뉴
-    for (let i = 0; i < gnbLi.length; i++) {
-        gnbLi[i].addEventListener('mouseenter', enter);
-        gnbLi[i].addEventListener('mouseleave', leave);
-    }
-
-    function enter(e) {
-        let li = e.target;
-        let gnbSub = li.querySelector('.sub');
-        if (gnbSub) {
-            gnbSub.classList.add('on');
-            header.classList.add('sub');
-        }
-    }
-
-    function leave(e) {
-        let li = e.target;
-        let gnbSub = li.querySelector('.sub');
-        if (gnbSub) {
-            gnbSub.classList.remove('on');
-            header.classList.remove('sub');
-        }
-    }
+    $(function(){
+        $("ul.sub").hide();
+        $(".gnb > .item").hover(function () {
+            $("ul:not(:animated)",this).slideDown("fast");
+        },
+        function () {
+            $("ul",this).slideUp("fast");
+        });
+    });
 
     // 헤더 우측 메뉴
     detailUtil.addEventListener('mouseenter', function () {
